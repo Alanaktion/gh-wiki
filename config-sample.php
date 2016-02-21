da111210d091186acee8949b34740ec8097739f7
Alan Hardman <alanaktion@gmail.com>
2016-02-21 15:57:44 -0700

    First commit!

---
+++ config-sample.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	"sitename" => "Wiki",
+    "db" => [
+        "host" => "localhost",
+        "username" => "root",
+        "password" => "",
+        "database" => "gh_wiki",
+    ],
+];
---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	"DEBUG" => 3,
+	"sitename" => "Totes a Wiki",
+    "db" => [
+        "host" => "localhost",
+        "username" => "root",
+        "password" => "ValleyForge",
+        "database" => "gh_wiki",
+    ],
+];
